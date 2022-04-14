d898b2eb0d25c761b80c95a7b54916f83a87b4ef
t-op2019 <votindu26062003@gmail.com>
2022-04-14 07:21:16 +0700

    Initial Commit

---
+++ Golf - big project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Golf - big project
+//
+//  Created by Dư Võ on 4/14/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
